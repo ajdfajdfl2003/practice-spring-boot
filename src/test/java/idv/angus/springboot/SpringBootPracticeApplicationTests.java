cb70917d29fb51add780e207cc602ac25389057c
anguswu <ajdfajdfl2003@gmail.com>
2021-06-06 15:42:45 +0800

    init project

---
+++ src/test/java/idv/angus/springboot/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.angus.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
